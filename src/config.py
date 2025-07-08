64f379c61e971817fd33bc23dedf27bfd621e754
nicolebid <nrbidwell@gmail.com>
2025-07-07 20:21:47 -0700

    file for paths

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+# Directories
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+RAW_AUDIO_DIR = os.path.join(ROOT_DIR, 'data/raw/english') 
